a386c3f8889bd17cfae650cbd81e5966a475125c
soureshmondal <117149085+soureshmondal@users.noreply.github.com>
2025-05-02 20:34:18 +0530

    Update magi.py

--- magi.py
+++ magi.py
@@ -1,9 +1,22 @@
-a = [1, 2, 3, 4, 5]
+rows = int(input("Enter number of rows: "))
 
-# Copying the list
-b = a.copy()
+k = 0
+count=0
+count1=0
 
-print(b)
-
-
-// Khanki magi souresh
+for i in range(1, rows+1):
+    for space in range(1, (rows-i)+1):
+        print("  ", end="")
+        count+=1
+    
+    while k!=((2*i)-1):
+        if count<=rows-1:
+            print(i+k, end=" ")
+            count+=1
+        else:
+            count1+=1
+            print(i+k-(2*count1), end=" ")
+        k += 1
+    
+    count1 = count = k = 0
+    print()
